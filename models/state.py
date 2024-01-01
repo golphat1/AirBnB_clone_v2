32d752d17f96d79911010bc97136e810e0a191bb
golphat1 <golphat@yahoo.com>
2024-01-01 12:46:13 -0800

    models

--- models/state.py
+++ models/state.py
@@ -6,3 +6,15 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """ Getter method to return the list of City objects """
+            from models.city import City
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
